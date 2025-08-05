f547c442f117b847df4cf922482556146399ee87
Pedro Henrick <pedro.henrickcs@gmail.com>
2025-08-04 21:36:45 -0300

    add cron jobs

--- app/routes/sync.tsx
+++
@@ -1,9 +0,0 @@
-import { type ActionFunctionArgs } from "@remix-run/router";
-import { syncMultipleRounds } from "~/lib/syncCartolaToFirestore";
-
-export async function action({ request }: ActionFunctionArgs) {
-  await syncMultipleRounds(18, 21);
-  return new Response(JSON.stringify({ ok: true }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}

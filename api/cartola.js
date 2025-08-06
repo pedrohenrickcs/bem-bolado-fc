ab22d9c462779feaaedae424e5653abb5b8a5d91
Pedro Henrick <pedro.henrickcs@gmail.com>
2025-08-06 11:31:08 -0300

    update version

--- api/cartola.js
+++ api/cartola.js
@@ -4,6 +4,7 @@ export default async function handler(req, res) {
   try {
     const response = await fetch(`https://api.cartola.globo.com${path}`);
     const data = await response.json();
+
     res.setHeader("Content-Type", "application/json");
     res.status(200).end(JSON.stringify(data));
   } catch (err) {
